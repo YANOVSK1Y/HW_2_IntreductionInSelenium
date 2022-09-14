26c79757ee14d100b5aa15b98e26ad7bd5c5ba93
Andrey <andreyianovskyi78@gmail.com>
2022-09-14 10:36:38 +0300

    Added FileReader for reading browser settings file. Add Browser Factory by switch case construction.

--- Utils/Driver.cs
+++ Utils/Driver.cs
@@ -1,18 +1,35 @@
-﻿using OpenQA.Selenium;
+﻿using Aquality.Selenium.Configurations.WebDriverSettings;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Runtime.CompilerServices;
 
 namespace HW_2_IntreductionInSelenium.Utils
 {
     public class Driver
     {
-        public static IWebDriver Instance; 
-
+        public static IWebDriver Instance;
+        private static JObject driverSetting = FileReader.ReadFile(@"Resources/driverSettings.json");
         public static IWebDriver getInstance()
         {
+            String browserName = driverSetting.GetValue("browser").ToString();
             if (Instance == null)
             {
-                Logger.getLogger().Info("Creating driver"); 
-                Instance = new ChromeDriver();
+                switch (browserName) {
+                    case "chrome":
+                        Instance = new ChromeDriver();
+                        Logger.getLogger().Info("Creating chrome driver");
+                        return Instance;
+                    case "firefox":
+                        Instance = new FirefoxDriver();
+                        Logger.getLogger().Info("Creating firefox driver");
+                        return Instance;
+                    default: 
+                        Instance = new ChromeDriver();
+                        Logger.getLogger().Info("Creating chrome driver by default");
+                        return Instance;
+                }
             }
             return Instance; 
         }
---
+++ Utils/FileReader.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace HW_2_IntreductionInSelenium.Utils
+{
+    public class FileReader
+    {
+        public static JObject ReadFile(string filePath)
+        {
+            using (StreamReader file = File.OpenText(filePath))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                return (JObject)JToken.ReadFrom(reader);
+            }
+        }
+    }
+}
